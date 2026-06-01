573ec2cbddaba84dd63185a09790db3444d417b3
Osc <ojulianpodium@gmail.com>
2026-06-01 13:47:06 +0200

    1 de junio 2026 v1

---
+++ 00 - CURS WEB/3 - UF1304/42 - FORMULARIS/js/42-form.js
@@ -0,0 +1,24 @@
+function fesclic(){
+    alert('Fes Click');
+}
+function DblClick(){
+    alert('FHe fet doble click sobre el boto');
+}
+function canvicolor(){
+    document.getElementById("caixa").style.backgroundColor="red";
+    document.getElementById("caixa").style.color="yellow";
+}
+function tornacolor(){
+    document.getElementById("caixa").style.backgroundColor="transparent";
+    document.getElementById("caixa").style.color="black";
+}
+function mostranumero(){
+    console.log(852)
+    document.getElementById("caixa").style.backgroundColor="orange";
+}
+function canviOnOff(){
+    alert('he apretat el boto del details');
+}
+function msgBenvinguda(){
+    alert('Bentornat a casa');
+}
